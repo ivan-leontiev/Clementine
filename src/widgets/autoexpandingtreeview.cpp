ivan-leontiev/Clementine

commit 22d68db190c822a91376697a0229a46d424380b9

Fix misbehavior when you double click after a single click on a tree item
This issue appeared as a side effect since the commit
bcaa9e4a37b732f91a36539e313937537be7aba2, which adds dropdown
animation to the library/internet view.

Reproduce:
If you single/double click on a song item in library/internet view
then all double clicks on this item will be ignored, until you
single click on it once again.

The cause was that if a tree item collapses(in response to your double click)
the "mouseDoubleClickEvent" gets called in the context where the view is in
"QAbstractTreeView::AnimatingState" state which makes impossible to emit
"doubleClicked" signal (see http://code.qt.io/cgit/qt/qt.git/tree/src/gui/itemviews/qtreeview.cpp#n1849).

To get rid of this behavior the code from "ItemDoubleClicked" slot
moved to the "mouseDoubleClickEvent" method.

src/widgets/autoexpandingtreeview.cpp
@@ -33,8 +33,6 @@ AutoExpandingTreeView::AutoExpandingTreeView(QWidget* parent)
 
   connect(this, SIGNAL(expanded(QModelIndex)), SLOT(ItemExpanded(QModelIndex)));
   connect(this, SIGNAL(clicked(QModelIndex)), SLOT(ItemClicked(QModelIndex)));
-  connect(this, SIGNAL(doubleClicked(QModelIndex)),
-          SLOT(ItemDoubleClicked(QModelIndex)));
 }
 
 void AutoExpandingTreeView::reset() {
@@ -84,7 +82,10 @@ void AutoExpandingTreeView::ItemClicked(const QModelIndex& index) {
   setExpanded(index, !isExpanded(index));
 }
 
-void AutoExpandingTreeView::ItemDoubleClicked(const QModelIndex& index) {
+void AutoExpandingTreeView::mouseDoubleClickEvent(QMouseEvent* event) {
+  QTreeView::mouseDoubleClickEvent(event);
+
+  QModelIndex index = indexAt(event->pos());
   ignore_next_click_ = true;
 
   if (add_on_double_click_) {

src/widgets/autoexpandingtreeview.h
@@ -48,6 +48,7 @@ signals:
   // QWidget
   void mousePressEvent(QMouseEvent* event);
   void keyPressEvent(QKeyEvent* event);
+  void mouseDoubleClickEvent(QMouseEvent* event);
 
   virtual bool CanRecursivelyExpand(const QModelIndex& index) const {
     return true;
@@ -56,7 +57,6 @@ signals:
  private slots:
   void ItemExpanded(const QModelIndex& index);
   void ItemClicked(const QModelIndex& index);
-  void ItemDoubleClicked(const QModelIndex& index);
 
  private:
   bool RecursivelyExpand(const QModelIndex& index, int* count);

commit 7d2580e85cd544508243c673f7ab24cb0c1d06ff

Oops, fix accidentally broken "Enter" key action
Fix "Enter" key behavior broken by the commit 22d68db190c822a91376697a0229a46d424380b9

src/widgets/autoexpandingtreeview.cpp
@@ -83,10 +83,7 @@ void AutoExpandingTreeView::ItemClicked(const QModelIndex& index) {
   setExpanded(index, !isExpanded(index));
 }
 
-void AutoExpandingTreeView::mouseDoubleClickEvent(QMouseEvent* event) {
-  QTreeView::mouseDoubleClickEvent(event);
-
-  QModelIndex index = indexAt(event->pos());
+void AutoExpandingTreeView::ItemDoubleClicked(const QModelIndex& index) {
   ignore_next_click_ = true;
 
   if (add_on_double_click_) {
@@ -96,6 +93,12 @@ void AutoExpandingTreeView::mouseDoubleClickEvent(QMouseEvent* event) {
     }
     emit AddToPlaylistSignal(data);
   }
+}
+
+void AutoExpandingTreeView::mouseDoubleClickEvent(QMouseEvent* event) {
+  QTreeView::mouseDoubleClickEvent(event);
+
+  ItemDoubleClicked(indexAt(event->pos()));
 }
 
 void AutoExpandingTreeView::mousePressEvent(QMouseEvent* event) {
@@ -116,10 +119,15 @@ void AutoExpandingTreeView::mousePressEvent(QMouseEvent* event) {
 }
 
 void AutoExpandingTreeView::keyPressEvent(QKeyEvent* e) {
+  QModelIndex index = currentIndex();
+
   switch (e->key()) {
     case Qt::Key_Enter:
     case Qt::Key_Return:
-      if (currentIndex().isValid()) emit doubleClicked(currentIndex());
+      if (index.isValid()) {
+        ItemDoubleClicked(index);
+        emit doubleClicked(index);
+      }
       e->accept();
       break;
 
@@ -130,8 +138,6 @@ void AutoExpandingTreeView::keyPressEvent(QKeyEvent* e) {
       break;
 
     case Qt::Key_Left:
-      QModelIndex index = currentIndex();
-
       // Set focus on the root of the current branch
       if (index.isValid() && index.parent() != rootIndex() &&
           (!isExpanded(index) || model()->rowCount(index) == 0)) {

src/widgets/autoexpandingtreeview.h
@@ -57,6 +57,7 @@ signals:
  private slots:
   void ItemExpanded(const QModelIndex& index);
   void ItemClicked(const QModelIndex& index);
+  void ItemDoubleClicked(const QModelIndex& index);
 
  private:
   bool RecursivelyExpand(const QModelIndex& index, int* count);